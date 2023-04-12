c9494ace9d11c750959f7d641d7f72eecf150104
cranis <cranis2@naver.com>
2023-04-12 14:15:43 +0900

    e3e3e3e3e3e

--- src/views/moviemarket.vue
+++ src/views/moviemarket.vue
@@ -4,25 +4,7 @@
       <div>
         <div>
           <br /><br /><br /><br /><br /><br /><br />
-          <div id="edxn" class="edxn0 aligntext">
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-              
-          </div>
+          <div id="edxn" class="edxn0 aligntext"></div>
         </div>
       </div>
     </div>
@@ -34,7 +16,31 @@ export default {
     return {};
   },
 
-  methods: {},
+  methods: {
+    basicstudy() {
+      let basic = document.getElementById("edxn");
+
+      basic.style.textAlign = "";
+    },
+  },
+
+  mounted() {
+    let basic = document.getElementById("edxn");
+
+    basic.style.inheritAttrs = false;
+
+    basic.classList.remove("wrapapp");
+
+    basic.style.textAlign = "justify";
+
+    console.log(basic.style);
+
+    console.dir(basic.style);
+
+    console.log(basic);
+
+    console.dir(basic);
+  },
 };
 </script>
 <style></style>
--- src/views/schedule.vue
+++ src/views/schedule.vue
@@ -1,94 +1,46 @@
 <template>
   <div>
-
-
-
     <div>
       <div>
         <div>
           <br /><br /><br /><br /><br /><br /><br />
-          <div id="edxn" class="edxn0 aligntext">
-
-
-
-
-
-
-
-
-
-
-
-
-
-              
-          </div>
+          <div id="edxn" class="edxn0 aligntext"></div>
         </div>
       </div>
     </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   </div>
 </template>
 <script>
-
-
-
-
 export default {
+  data() {
+    return {};
+  },
 
+  methods: {
+    basicstudy() {
+      let basic = document.getElementById("edxn");
 
-    data() {
-        return {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        }
+      basic.style.textAlign = "";
     },
+  },
 
+  mounted() {
+    let basic = document.getElementById("edxn");
 
-methods: {
+    basic.style.inheritAttrs = false;
 
+    basic.classList.remove("wrapapp");
 
+    basic.style.textAlign = "justify";
 
+    console.log(basic.style);
 
+    console.dir(basic.style);
 
+    console.log(basic);
 
-
-
-
-}    
-}
-
+    console.dir(basic);
+  },
+};
 </script>
-<style>
-
-</style>
+<style></style>
