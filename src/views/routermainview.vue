7de45e21a1f7b3f4172e331d3beb075a9a30bbaa
cranis <cranis2@naver.com>
2023-11-05 18:56:34 +0900

    eeffeffeefeffeefeffefefefe

--- src/views/routermainview.vue
+++ src/views/routermainview.vue
@@ -1,7 +1,7 @@
 <template>
   <v-main class="ma-0 pa-0" style="margin-bottom: 0px">
-    <v-container class="setcolor fullhgt" fluid style="margin-bottom: 0px;">
-      <router-view class="fullhgt setzindex wrapapp"> </router-view>
+    <v-container class="fullhgt" fluid style="margin-bottom: 0px;">
+      <router-view class="fullhgt wrapapp"> </router-view>
     </v-container>
   </v-main>
 </template>
@@ -10,13 +10,13 @@ export default {};
 </script>
 
 <style scoped>
-.setcolor {
+/* .setcolor {
   background: rgb(255, 203, 90)
 }
 
 .setzindex {
   background: white;
-}
+} */
 
 .fullhgt {
   height: 100%;
