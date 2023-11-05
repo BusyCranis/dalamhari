99c687d6af46da65675dc70da33b6c6f7f778108
cranis <cranis2@naver.com>
2023-11-05 19:10:08 +0900

    effefeefeffeefefefeffefeefeffefefefefe

--- src/views/routermainview.vue
+++ src/views/routermainview.vue
@@ -1,6 +1,7 @@
 <template>
   <v-main class="ma-0 pa-0" style="margin-bottom: 0px">
-    <v-container class="fullhgt" fluid style="margin-bottom: 0px;">
+    <v-container class="ma-0 pa-0 fullhgt" fluid style="margin-bottom: 0px">
+      <br /><br /><br /><br />
       <router-view class="fullhgt wrapapp"> </router-view>
     </v-container>
   </v-main>
