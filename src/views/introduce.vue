80962d7e5e4ff8af7f09560a577dd85065f5ebba
cranis <cranis2@naver.com>
2023-11-06 18:34:16 +0900

    efeffefefeefefefefeffefeefeffeefefefeffefe

--- src/views/introduce.vue
+++ src/views/introduce.vue
@@ -1,30 +1,47 @@
 <template>
-  <div id="whybartool" class="set101color" transparent @click.self="detectAction">
-    <input   id="nameinput"   style="display: none" />
-    <input   id="emailinput"   style="display: none" />
+  <div
+    id="whybartool"
+    class="set101color"
+    transparent
+    @click.self="detectAction"
+  >
+    <input id="nameinput" style="display: none" />
+    <input id="emailinput" style="display: none" />
     <br />
-    <input   id="passwordinput"   style="display: none" />
+    <input id="passwordinput" style="display: none" />
 
-    <v-menu          id="identityinput">
-        <template v-slot:activator="{ on, attrs }">
-          <div
-
-            id="specselection"
-            v-bind="attrs"
-            v-on="on"
-            class="elevation-0 ma-0 pa-0 textcolor"
-            color="transparent"
-          >
-            기관 소개
-          </div>
-        </template>
-        <br /><br />
-        <v-list>
-          <v-list-item >인사말</v-list-item>
-          <v-list-item >협회 정보</v-list-item>
-          <v-list-item >찾아오시는 길</v-list-item>
-        </v-list>
-      </v-menu>
+    <v-menu id="identityinput">
+      <template v-slot:activator="{ on, attrs }">
+        <div
+          id="specselection"
+          v-bind="attrs"
+          v-on="on"
+          class="elevation-0 ma-0 pa-0 textcolor"
+          color="transparent"
+        >
+          기관 소개
+        </div>
+      </template>
+      <br /><br />
+      <v-list>
+        <v-list-item value="Individual Seller : I sell on Amazon by myself"
+          >Individual Seller : I sell on Amazon by myself</v-list-item
+        >
+        <v-list-item
+          value="Brand : I own private label brand products ans sell on Amazon"
+          >Brand : I own private label brand products ans sell on
+          Amazon</v-list-item
+        >
+        <v-list-item
+          value="Consultant : I work with multiple brands & sellers on Amazon"
+          >Consultant : I work with multiple brands & sellers on
+          Amazon</v-list-item
+        >
+        <v-list-item value="I am not selling on Amazon yet"
+          >I am not selling on Amazon yet</v-list-item
+        >
+      </v-list>
+    </v-menu>
 
     <br />
     <br />
@@ -71,8 +88,7 @@ export default {
       emailinput: null,
       passwordinput: null,
       identityinput: null,
-      specselection: null
-
+      specselection: null,
     };
   },
 
@@ -92,28 +108,26 @@ export default {
       // this.nameinput.style.display = ""
       // "너비비율= 0.2951219512195122,  높이비율= 0.42916666666666664"
 
-      this.nameinput.style.transform = `translate3d( ${event.target.clientWidth * 0.2975609756}px, ${event.target.clientHeight * 0.298}px, 0 )`
-   
-      this.emailinput.style.transform = `translate3d( ${event.target.clientWidth * 0.2975609756 - 167}px, ${event.target.clientHeight * 0.3605416666}px, 0 )`
+      this.nameinput.style.transform = `translate3d( ${
+        event.target.clientWidth * 0.2975609756
+      }px, ${event.target.clientHeight * 0.298}px, 0 )`;
 
-      this.passwordinput.style.transform = `translate3d( ${event.target.clientWidth * 0.2975609756}px, ${event.target.clientHeight * 0.3981666666}px, 0 )`
+      this.emailinput.style.transform = `translate3d( ${
+        event.target.clientWidth * 0.2975609756 - 167
+      }px, ${event.target.clientHeight * 0.3605416666}px, 0 )`;
 
+      this.passwordinput.style.transform = `translate3d( ${
+        event.target.clientWidth * 0.2975609756
+      }px, ${event.target.clientHeight * 0.3981666666}px, 0 )`;
 
-      this.nameinput.style.display = ""
-      this.emailinput.style.display = ""
-      this.passwordinput.style.display = ""
-
-
+      this.nameinput.style.display = "";
+      this.emailinput.style.display = "";
+      this.passwordinput.style.display = "";
 
       // this.identityinput.style.transform = `translate3d( ${event.target.clientWidth * 0.2975609756}px, ${event.target.clientHeight * 0.298}px, 0 )`
       // this.identityinput.style.display = ""
 
-
-
-      console.log(this.passwordinput)
-
-
-
+      console.log(this.passwordinput);
     },
 
     basicstudy() {
@@ -124,24 +138,22 @@ export default {
   },
 
   mounted() {
-    this.nameinput = document.getElementById("nameinput")
-    console.log(this.nameinput)
+    this.nameinput = document.getElementById("nameinput");
+    console.log(this.nameinput);
 
-    this.emailinput = document.getElementById("emailinput")
-    console.log(this.emailinput)
+    this.emailinput = document.getElementById("emailinput");
+    console.log(this.emailinput);
 
-    this.passwordinput = document.getElementById("passwordinput")
-    console.log(this.passwordinput)
+    this.passwordinput = document.getElementById("passwordinput");
+    console.log(this.passwordinput);
 
-    this.identityinput = document.getElementById("identityinput")
-    console.log(this.identityinput)
+    this.identityinput = document.getElementById("identityinput");
+    console.log(this.identityinput);
 
+    this.specselection = document.getElementById("specselection");
 
-
-
-
-
-
+    console.log(this.specselection);
+    console.log(this.specselection.textContent);
   },
 };
 </script>
