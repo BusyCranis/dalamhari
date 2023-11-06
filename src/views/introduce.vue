d5c71a5b9c4bf56c4717813b071faa78470af964
cranis <cranis2@naver.com>
2023-11-06 10:57:57 +0900

    effefefefeefeffefefeeffeeffeeefefeffe

--- src/views/introduce.vue
+++ src/views/introduce.vue
@@ -1,16 +1,7 @@
 <template>
-  <div 
-    id="whybartool"
-    class="set101color"
-    transparent
-    
-  
-    @click="detectAction"
-  
-  
-  
-  
-  >
+  <div id="whybartool" class="set101color" transparent @click.self="detectAction">
+    <input   id="nameinput"   style="display: none" />
+
     <br />
     <br />
     <br />
@@ -46,51 +37,49 @@
     <br />
     <br />
     <br />
-    
-   
-    
-
-
-
-
-    
-   
-
-
-
-
-
-
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+
+      nameinput: null
+
+    };
   },
 
   methods: {
-
-
     detectAction(event) {
       event.stopPropagation();
 
-      console.log(event.target)      
-      let posXrate = event.offsetX / event.target.clientWidth
-      let posYrate = event.offsetY / event.target.clientHeight
+      console.log(event.target);
+      let posXrate = event.offsetX / event.target.clientWidth;
+      let posYrate = event.offsetY / event.target.clientHeight;
 
-      console.log("너비비율= " + posXrate + ",  높이비율= " + posYrate)
+      console.log("너비비율= " + posXrate + ",  높이비율= " + posYrate);
 
       // if(posXrate > 0.8512195 && posYrate > 0.2340425 && posXrate < 0.9536585 && posYrate < 0.7234042) {
       //   console.log("클릭")
       // }
 
+      // this.nameinput.style.display = ""
+
+      // "너비비율= 0.4975609756097561,  높이비율= 0.728125"
+
+
+      this.nameinput.style.transform = `translate3d( ${event.target.clientWidth * 0.2975609756}px, ${event.target.clientHeight * 0.298}px, 0 )`
+
+
+
+
+      this.nameinput.style.display = ""
+
+
+
+
 
     },
-
-
-
-
 
     basicstudy() {
       let basic = document.getElementById("edxn");
@@ -100,20 +89,16 @@ export default {
   },
 
   mounted() {
-    // let basic = document.getElementById("edxn");
-    // basic.style.inheritAttrs = false;
-    // basic.classList.remove("wrapapp");
-    // basic.style.textAlign = "justify";
-    // console.log(basic.style);
-    // console.dir(basic.style);
-    // console.log(basic);
-    // console.dir(basic);
+    this.nameinput = document.getElementById("nameinput")
+
+    console.log(this.nameinput)
+
+
+
   },
 };
 </script>
 <style scoped>
-
-
 .set101color {
   background-image: url("/01-2 Home-Sign Up.png");
 
@@ -123,22 +108,12 @@ export default {
 
   /* background-size: 100% 50%; */
 
-  background-size: 100% 100%; 
+  background-size: 100% 100%;
 
   background-position: center;
 
   /* background-position: top; */
 }
-
-
-
-
-
-
-
-
-
-
 
 .edxn0 {
   inherits: false;
